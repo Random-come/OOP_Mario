f32cafef3dd882c6441708fc0d93fc436a6fd9d4
Phan Vũ Tuấn Khanh <phankhanh14052005@gmail.com>
2024-11-19 23:14:28 +0700

    add main package and directory

--- src/brosject/Brosject.java
+++
@@ -1,8 +0,0 @@
-package brosject;
-public class Brosject {
-    public static void main(String[] args) {
-        System.out.println("hello");
-        System.out.println("Test Git last hope");
-        System.out.println("Test Git last hope 2");
-    }
-}
