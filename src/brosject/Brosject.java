391222acfc5fbcf4ddf5184377584e4018ca23c2
Random-come <nhattanph250205@gmail.com>
2024-11-19 22:20:11 +0700

    Tôi muốn test 1 vài thứ lần 2

--- src/brosject/Brosject.java
+++ src/brosject/Brosject.java
@@ -3,5 +3,6 @@ public class Brosject {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("Test Git last hope");
+        System.out.println("Test Git last hope 2");
     }
 }
