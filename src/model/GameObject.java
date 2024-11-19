be51babc1a49188ea57c5f63ca38032cc171c8a1
Phan Vũ Tuấn Khanh <phankhanh14052005@gmail.com>
2024-11-19 23:26:42 +0700

    add main package and directory

---
+++ src/model/GameObject.java
@@ -0,0 +1,15 @@
+package model;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class GameObject {
+    private double x,y;
+    private double velX, velY;
+    private Dimension dimension;
+    private BufferedImage style;
+    private double gravityAcc;
+    private boolean falling, jumping = false;
+
+
+}
