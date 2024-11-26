eb39329ec338c7fceb73ef5697c5b6151ec16ac3
Phan Vũ Tuấn Khanh <phankhanh14052005@gmail.com>
2024-11-27 00:12:48 +0700

    change back GameObject

--- src/model/GameObject.java
+++ src/model/GameObject.java
@@ -1,128 +1,155 @@
+package model;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import javax.swing.*;
-public class GameObject {
-    private double x;
-    private double y;
-    private boolean falling;
-    private boolean jumping;
-    private BufferedImage style;
-    private double velocX; //velocity of vertical
-    private double velocY; //velocity of horizontal
+
+public abstract class GameObject {
+
+    private double x, y;
+    private double velX, velY;
     private Dimension dimension;
-    private double gravity;
-    
+    private BufferedImage style;
+    private double gravityAcc;
+    private boolean falling, jumping;
+
+    //constructor
     public GameObject(double x, double y, BufferedImage style){
-        setLocation(x,y);
+        setLocation(x, y);
         setStyle(style);
-        setDimension(style.getWidth(),style.getHeight());
-        setVelocX(0);
-        setVelocY(0);
-        setGravity(0.55);
-    }   
-
-    public double getX(){
-        return x;
-    }
-    public void setX(double x){
-        this.x=x;
+        if(style != null){
+            setDimension(style.getWidth(), style.getHeight());}
+        setVelX(0);
+        setVelY(0);
+        setGravityAcc(0.38);
+        jumping = false;
+        falling = true;
     }
 
-    public double getY(){
-        return x;
-    }
-    public void setY(double y){
-        this.y=y;
-    }
-
-    public boolean isFalling(){
-        return falling;
-    }
-    public void setFalling(boolean falling){
-        this.falling=falling;
-    }
-
-    public boolean isJumping(){
-        return falling;
-    }
-    public void setJumping(boolean jumping){
-        this.jumping=jumping;
-    }    
-
-    public BufferedImage getStyle(){
-        return style;
-    }
-    public void setStyle(BufferedImage style){
-        this.style=style;
+    public void draw(Graphics g) {//render object
+        BufferedImage style = getStyle();
+        if(style != null){
+            g.drawImage(style, (int)x, (int)y, null);
+        }
+        //for debugging
+        /*Graphics2D g2 = (Graphics2D)g;
+        g2.setColor(Color.WHITE);
+        g2.draw(getTopBounds());
+        g2.draw(getBottomBounds());
+        g2.draw(getRightBounds());
+        g2.draw(getLeftBounds());*/
     }
 
-    public double getVelocX(){
-        return velocX;
-    }
-    public void setVelocX(double velocX){
-        this.velocX=velocX;
+    public void updateLocation() {
+        if(jumping && velY <= 0){//reach the peak of jumping -> falling
+            jumping = false;
+            falling = true;}
+        else if(jumping){//while jumping state
+            velY = velY - gravityAcc;
+            y = y - velY;}
+        if(falling){//while falling state
+            y = y + velY;
+            velY = velY + gravityAcc;}
+
+        x = x + velX;
     }
 
-    public double getVelocY(){
-        return velocX;
-    }
-    public void setVelocY(double velocY){
-        this.velocY=velocY;
-    }
-
+    //Getter and Setter
     public void setLocation(double x, double y) {
         setX(x);
         setY(y);
     }
 
-    public void changeLocation(){
-        if (jumping && getVelocX()<=0){ // check if the object currently at the peak of the jump then falling down
-            setJumping(false);
-            setFalling(true); //begin falling down
-        }
+    public double getX() {
+        return x;
+    }
 
-        if (jumping){
-            setVelocY(0.69); //jumping with velocity of 0.69
-        }
+    public void setX(double x) {
+        this.x = x;
+    }
 
-        if (falling){
-            setVelocY(0.89); //falling down with velocity 0.89
-        }
-        x = x + getVelocX();  //update vertical position of object
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
     }
 
     public Dimension getDimension(){
         return dimension;
     }
-    public void setDimension(int width,int height){
-        this.dimension=new Dimension(width,height); //get width and height method from Dimension library
+
+    public void setDimension(Dimension dimension) {
+        this.dimension = dimension;
     }
 
-    public double getGravity(){
-        return gravity;
-    }
-    public void setGravity(double gravity){
-        this.gravity=gravity;
+    public void setDimension(int width, int height){ this.dimension =  new Dimension(width, height); }
+
+    public BufferedImage getStyle() {
+        return style;
     }
 
-    public Rectangle getBounds(){ //create a rectangle to check if the object touch any other object
-        return new Rectangle((int)x, (int)y, dimension.width, dimension.height);
+    public void setStyle(BufferedImage style) {
+        this.style = style;
     }
 
-    public Rectangle getTopBounds(){ //the ceiling
+    public double getVelX() {
+        return velX;
+    }
+
+    public void setVelX(double velX) {
+        this.velX = velX;
+    }
+
+    public double getVelY() {
+        return velY;
+    }
+
+    public void setVelY(double velY) {
+        this.velY = velY;
+    }
+
+    public double getGravityAcc() {
+        return gravityAcc;
+    }
+
+    public void setGravityAcc(double gravityAcc) {
+        this.gravityAcc = gravityAcc;
+    }
+
+    public Rectangle getTopBounds(){
         return new Rectangle((int)x+dimension.width/6, (int)y, 2*dimension.width/3, dimension.height/2);
     }
 
-    public Rectangle getBottomBounds(){ //the ground
+    public Rectangle getBottomBounds(){
         return new Rectangle((int)x+dimension.width/6, (int)y + dimension.height/2, 2*dimension.width/3, dimension.height/2);
     }
 
-    public Rectangle getLeftBounds(){ //the left wall
+    public Rectangle getLeftBounds(){
         return new Rectangle((int)x, (int)y + dimension.height/4, dimension.width/4, dimension.height/2);
     }
 
-    public Rectangle getRightBounds(){ //the rigth wall
+    public Rectangle getRightBounds(){
         return new Rectangle((int)x + 3*dimension.width/4, (int)y + dimension.height/4, dimension.width/4, dimension.height/2);
+    }
+
+    public Rectangle getBounds(){
+        return new Rectangle((int)x, (int)y, dimension.width, dimension.height);
+    }
+
+    public boolean isFalling() {
+        return falling;
+    }
+
+    public void setFalling(boolean falling) {
+        this.falling = falling;
+    }
+
+    public boolean isJumping() {
+        return jumping;
+    }
+
+    public void setJumping(boolean jumping) {
+        this.jumping = jumping;
     }
 }
