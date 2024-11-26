29befd1c8ede42cf6e9f5d286cd63a2b78c2017a
Phan Vũ Tuấn Khanh <phankhanh14052005@gmail.com>
2024-11-27 00:14:31 +0700

    create BoostItem

---
+++ src/model/prize/BoostItem.java
@@ -0,0 +1,52 @@
+package model.prize;
+
+import manager.GameEngine;
+import manager.MapManager;
+import model.GameObject;
+import model.hero.Mario;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public abstract class BoostItem extends GameObject implements Prize{
+
+    private boolean revealed = false;//indicates if the following object has been revealed to player, initially: False
+    private int point;//number of points player earns when collecting this item.
+
+
+    public BoostItem(double x, double y, BufferedImage style) {
+        super(x, y, style);
+        setDimension(48, 48);
+    }
+
+    public abstract void onTouch(Mario mario, GameEngine engine);//check collision
+
+    @Override
+    public int getPoint() {
+        return point;
+    }
+
+    @Override
+    public void updateLocation(){
+        if(revealed){
+            super.updateLocation();
+        }
+    }
+
+    @Override
+    public void draw(Graphics g){//render
+        if(revealed){
+            g.drawImage(getStyle(), (int)getX(), (int)getY(), null);
+        }
+    }
+
+    @Override
+    public void reveal(){//reveal the item by moving it up 48 pixels
+        setY(getY()-48);
+        revealed = true;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
+}
