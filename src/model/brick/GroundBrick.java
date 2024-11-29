658be5bb40de7470a4ab38ca8d9fe3267b4d0f1f
Phước Huỳnh <phuochuynh120@gmail.com>
2024-11-30 01:36:19 +0700

    GroundBrick add in

---
+++ src/model/brick/GroundBrick.java
@@ -0,0 +1,8 @@
+package model.brick;
+import java.awt.image.BufferedImage;
+import model.GameObject;
+public class GroundBrick extends GameObject{
+    public GroundBrick(double x,double y,BufferedImage style){
+        super(x,y,style);
+    }
+}
