c58b9a44f591051f4a979cb2e8f2a9f804e0fcd0
Phước Huỳnh <phuochuynh120@gmail.com>
2024-11-30 01:53:25 +0700

    change in turtle.enemy

---
+++ src/model/Enemy/Turtle.java
@@ -0,0 +1,26 @@
+package model.enemy;
+import java.awt.image.BufferedImage;
+import java.awt.*;
+public class Turtle extends Enemy {
+    private BufferedImage image;
+    public Turtle(double x,double y,BufferedImage style){
+        super(x,y,style);
+        setVelY(0.45);
+    }
+    
+    public BufferedImage image(){
+        return image;
+    }
+    public void setRightImage(BufferedImage image) {
+        this.image = image;
+    }
+
+    @Override
+    public void draw(Graphics g){
+        if(getVelX() > 0){
+            g.drawImage(image, (int)getX(), (int)getY(), null); //image move if turtle is moving
+        }
+        else
+            super.draw(g); //turtle stop at 1 place
+    }
+}
--- src/model/brick/Brick.java
+++ src/model/brick/Brick.java
@@ -1,1 +1,1 @@
-this is brick
+package model.brick;
