677126ae5b8cf25ea800de7ab5082ff2c89435c7
Phan Vũ Tuấn Khanh <phankhanh14052005@gmail.com>
2024-12-01 21:33:35 +0700

    fix some bug

--- src/model/Enemy/Enemy.java
+++ src/model/Enemy/Enemy.java
@@ -1,5 +1,8 @@
+package model.enemy;
+
 import model.GameObject;
 import java.awt.image.BufferedImage;
+
 public class Enemy extends GameObject{
     public Enemy(double x,double y,BufferedImage style){
         super(x,y);
