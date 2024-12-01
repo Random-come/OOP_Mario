faaa53c45986889288aab4429f469fc987015b79
Phước Huỳnh <phuochuynh120@gmail.com>
2024-12-01 21:27:29 +0700

    add style in enemy class

--- src/model/Enemy/Enemy.java
+++ src/model/Enemy/Enemy.java
@@ -1,7 +1,7 @@
 import model.GameObject;
 import java.awt.image.BufferedImage;
 public class Enemy extends GameObject{
-    public Enemy(double x,double y){
+    public Enemy(double x,double y,BufferedImage style){
         super(x,y);
         setFalling(false);
         setJumping(false);
