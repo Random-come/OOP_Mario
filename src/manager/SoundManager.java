0f2f5cb4901a56eeacfab1a922d88d0ca5e11d47
Phước Huỳnh <phuochuynh120@gmail.com>
2024-12-01 23:03:09 +0700

    add in

--- src/manager/SoundManager.java
+++ src/manager/SoundManager.java
@@ -14,7 +14,7 @@ public class SoundManager {
     private FloatControl backgroundControl;
 
     public SoundManager() {
-        background = getClip(loadAudio("thien ly oi"));
+        background = getClip(loadAudio("background"));
         if (background != null) {
             backgroundControl = (FloatControl) background.getControl(FloatControl.Type.MASTER_GAIN);
         }
