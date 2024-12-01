b409e87e024a297411fbf34da653f68d5d74e1ed
Random-come <nhattanph250205@gmail.com>
2024-12-01 21:55:10 +0700

    Change sth1

--- src/view/StartScreenSelection.java
+++ src/view/StartScreenSelection.java
@@ -14,27 +14,34 @@ public enum StartScreenSelection {
     VIEW_ABOUT(2);
 
     private final int lineNumber;
-    StartScreenSelection(int lineNumber){ this.lineNumber = lineNumber; }
 
-    public StartScreenSelection getSelection(int number){
-        if(number == 0)
-            return START_GAME;
-        else if(number == 1)
-            return VIEW_HELP;
-        else if(number == 2)
-            return VIEW_ABOUT;
-        else return null;
+    StartScreenSelection(int lineNumber) {
+        this.lineNumber = lineNumber;
     }
 
-    public StartScreenSelection select(boolean toUp){
+    public StartScreenSelection getSelection(int number) {
+        if (number == 0) {
+            return START_GAME;
+        } else if (number == 1) {
+            return VIEW_HELP;
+        } else if (number == 2) {
+            return VIEW_ABOUT;
+        } else {
+            return null;
+        }
+
+    }
+
+    public StartScreenSelection select(boolean toUp) {
         int selection;
 
-        if(lineNumber > -1 && lineNumber < 3){
+        if (lineNumber > -1 && lineNumber < 3) {
             selection = lineNumber - (toUp ? 1 : -1);
-            if(selection == -1)
+            if (selection == -1) {
                 selection = 2;
-            else if(selection == 3)
+            } else if (selection == 3) {
                 selection = 0;
+            }
             return getSelection(selection);
         }
 
