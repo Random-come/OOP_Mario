74dd1de5b3f4c2bc58c3ef7361ef8a16f52467e4
Phan Vũ Tuấn Khanh <phankhanh14052005@gmail.com>
2024-12-08 23:11:11 +0700

    fix icon in khanh way

--- src/manager/GameEngine.java
+++ src/manager/GameEngine.java
@@ -342,9 +342,11 @@ public class GameEngine extends javax.swing.JFrame implements Runnable {
 
     }
 
-    private void setIconImage(Image image) {
+    public void setIconImage(Image image) {
         setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getClassLoader().getResource("media/Bros.png")));
     }
+
+
 
     public static void main(String... args) {
 
